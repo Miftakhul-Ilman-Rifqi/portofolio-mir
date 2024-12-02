7128bdaed98557a18b7e3f0e32d3620cd228ea67
Miftakhul Ilman Rifqi <bozzclax@gmail.com>
2024-12-02 22:29:53 +0700

    add aceternity-ui setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,43 @@
 import type { Config } from "tailwindcss";
+import flattenColorPalette from "tailwindcss/lib/util/flattenColorPalette";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    darkMode: "class",
+    theme: {
+        extend: {
+            colors: {
+                background: "var(--background)",
+                foreground: "var(--foreground)",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [addVariablesForColors],
 };
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({
+    addBase,
+    theme,
+}: {
+    addBase: (base: Record<string, Record<string, string>>) => void;
+    theme: (path: string) => Record<string, string>;
+}) {
+    const allColors = flattenColorPalette(theme("colors")) as Record<
+        string,
+        string
+    >;
+    const newVars: Record<string, string> = Object.fromEntries(
+        Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+    );
+
+    addBase({
+        ":root": newVars,
+    });
+}
+
 export default config;
