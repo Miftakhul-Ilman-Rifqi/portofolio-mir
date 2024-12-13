c2d40aee3d1bd5258758b453b121866daa0f7d29
Miftakhul Ilman Rifqi <bozzclax@gmail.com>
2024-12-14 02:50:37 +0700

    add handleClick link resume

--- src/components/common/ButtonResume.tsx
+++ src/components/common/ButtonResume.tsx
@@ -3,12 +3,20 @@ import { HoverBorderGradient } from "../ui/hover-border-gradient";
 import Image from "next/image";
 
 export function ButtonResume() {
+    const handleClick = () => {
+        window.open(
+            "https://drive.google.com/drive/folders/1V8BxZ86AjQsfq1DIOxbagRgpCmDyQNuc?usp=sharing",
+            "_blank"
+        );
+    };
+
     return (
         <div className="flex justify-center text-center pt-3">
             <HoverBorderGradient
                 containerClassName="rounded-full"
                 as="button"
                 className="dark:bg-black dark:text-white flex items-center space-x-2"
+                onClick={handleClick}
             >
                 <Image
                     src="/assets/icon/cv1.webp"
